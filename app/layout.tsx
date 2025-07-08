2d7ca25d4fb9fdd46bbf530f5038eebfdfd1edf1
unknown <fmrigueros91@gmail.com>
2025-07-08 17:31:16 -0500

    Añadiendo Analytics de vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,43 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "@/components/theme-provider"
-import { LanguageProvider } from "@/contexts/LanguageContext"
-
-const inter = Inter({ subsets: ["latin"] })
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import { Analytics } from "@vercel/analytics/react";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SPM INTEGRAL - Soluciones Digitales Estratégicas",
   description:
     "Transformamos ideas en soluciones digitales de alto impacto. Desarrollo web, apps móviles, cloud services y más.",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           <LanguageProvider>
             <Header />
             <main>{children}</main>
+            <Analytics />
             <Footer />
           </LanguageProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
